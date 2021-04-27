36124824a039e7864375d11a9c18673401910ea4
erojassc <61214703+erojassc@users.noreply.github.com>
2021-04-27 04:35:11 -0600

    actualizacion de datos

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+var cors = require('cors')
 //setting
-app.set('port',process.env.PORT || 3000);
+app.set('port',process.env.PORT || 3021);
 app.set('json space', 2);
 
 //middlewares
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cors());
 //routes
 app.use(require('./routes/contact_routes'));
 //starting server
